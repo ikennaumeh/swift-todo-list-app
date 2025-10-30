f79c44c214f5c3ba105ea34260eabbf75bde2a48
ikenna <kennaumeh@gmail.com>
2025-10-30 16:21:19 +0100

    Initial Commit

---
+++ swift-todo-list/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-todo-list
+//
+//  Created by Ikenna Umeh on 30/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-todo-list/swift_todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_todo_listApp.swift
+//  swift-todo-list
+//
+//  Created by Ikenna Umeh on 30/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct swift_todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
